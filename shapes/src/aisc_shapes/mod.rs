bb65c1032cd031738e3e03cf1d74f320d70c0a71
David Brungardt <34960989+brungardtdb@users.noreply.github.com>
2025-09-18 18:38:11 -0600

    Remove Generic AISC Shape

--- shapes/src/aisc_shapes/mod.rs
+++ shapes/src/aisc_shapes/mod.rs
@@ -1,42 +1,39 @@
 //! This module is responsible for creating and modeling
 //! AISC steel shapes
 
-/// Generic AISC Shape, will be removed in the future
-pub mod aisc_shape;
 /// Manages errors for constructing steel profiles
 pub mod errors;
 /// Manages the construction of steel shapes
 pub mod shape_builder;
-/// Struct that models wide flange steel profiles
+/// Models wide flange steel profiles
 pub mod wide_flange;
-/// Struct that models misc beams or "m" steel profiles
+/// Models misc beams or "m" steel profiles
 pub mod misc_beam;
-/// Struct that models structural beams or "s" steel profiles
+/// Models structural beams or "s" steel profiles
 pub mod structural_beam;
-/// Struct that models h-pile beams or "hp" steel profiles
+/// Models h-pile beams or "hp" steel profiles
 pub mod h_pile;
-/// Strut that models cee channels 
+/// Models cee channels 
 pub mod cee_channel;
-/// Struct that models miscelaneous channels
+/// Models miscelaneous channels
 pub mod misc_channel;
-/// Struct that models hot-rolled angles
+/// Models hot-rolled angles
 pub mod angle;
-/// Struct that models wide-flange tee shapes
+/// Models wide-flange tee shapes
 pub mod wide_flange_tee;
-/// Struct that models misc. tee shapes
+/// Models misc. tee shapes
 pub mod misc_tee;
-/// Struct that models structural tee shapes
+/// Models structural tee shapes
 pub mod structural_tee;
-/// Struct that models double angle shapes
+/// Models double angle shapes
 pub mod double_angle;
-/// Struct that models square and rectangular HSS shapes
+/// Models square and rectangular HSS shapes
 pub mod hollow_structural_section;
-/// Struct that models round HSS shapes
+/// Models round HSS shapes
 pub mod round_hollow_structural_section;
 ///Struct that models pipe shapes
 pub mod pipe;
 
-pub use self::aisc_shape::AISCShape;
 pub use self::errors::MissingPropertyError;
 pub use self::shape_builder::ShapeBuilder;
 pub use self::wide_flange::WideFlange;
