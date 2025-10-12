cd46cf6cae4ea11e41abc662aa85b6cd115dc116
David Brungardt <34960989+brungardtdb@users.noreply.github.com>
2025-10-11 19:09:00 -0600

    Create Misc. Tee Repository

--- shape_repositories/src/repositories/mod.rs
+++ shape_repositories/src/repositories/mod.rs
@@ -17,6 +17,9 @@ pub mod misc_beam_repository;
 /// managing data access for misc. channels
 pub mod misc_channel_repository;
 /// The repository responsible for
+/// managing data access for misc. tee shapes
+pub mod misc_tee_repository;
+/// The repository responsible for
 /// managing data access for structural beam shapes
 pub mod structural_beam_repository;
 /// The repository responsible for
@@ -31,6 +34,7 @@ pub use cee_channel_repository::CeeChannelRepository;
 pub use h_pile_repository::HPileRepository;
 pub use misc_beam_repository::MiscBeamRepository;
 pub use misc_channel_repository::MiscChannelRepository;
+pub use misc_tee_repository::MiscTeeRepository;
 pub use structural_beam_repository::StructuralBeamRepository;
 pub use wide_flange_repository::WideFlangeRepository;
 pub use wide_flange_tee_repository::WideFlangeTeeRepository;
--- shapes_api/src/main.rs
+++ shapes_api/src/main.rs
@@ -1,4 +1,4 @@
-use shape_repositories::repositories::{WideFlangeTeeRepository};
+use shape_repositories::repositories::{MiscTeeRepository};
 use shapes::aisc_shapes::shape_repository::ShapeRepository;
 use std::sync::Arc;
 
@@ -8,7 +8,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         std::env::var("DATABASE_URL").expect("Env var DATABASE_URL is required for this example.");
     let pool = sqlx::PgPool::connect(&conn_str).await?;
     let conx = Arc::new(pool);
-    let repo = WideFlangeTeeRepository::new(Arc::clone(&conx));
+    let repo = MiscTeeRepository::new(Arc::clone(&conx));
     let all_shapes_result = repo.all().await;
     match all_shapes_result {
         Ok(shapes) => println!("There are {} wide flange tees", shapes.len()),
@@ -16,20 +16,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     let edi_result = repo
-        .shape_with_edi_std_nomenclature(String::from("WT6X8"))
+        .shape_with_edi_std_nomenclature(String::from("MT6X5"))
         .await;
     match edi_result {
         Ok(shape) => println!("{}", shape.edi_std_nomenclature),
         Err(err) => println!("{}", err),
     }
     let lbl_result = repo
-        .shape_with_aisc_manual_label(String::from("WT6X8"))
+        .shape_with_aisc_manual_label(String::from("MT6X5"))
         .await;
     match lbl_result {
         Ok(shape) => println!("{}", shape.aisc_manual_label),
         Err(err) => println!("{}", err),
     }
-    let depth_result = repo.shapes_with_depth(8.01).await;
+    let depth_result = repo.shapes_with_depth(5.0).await;
     match depth_result {
         Ok(shapes) => {
             for shape in shapes {
@@ -38,7 +38,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         Err(err) => println!("{}", err),
     }
-    let shapes_result = repo.shapes_with_width(17.0).await;
+    let shapes_result = repo.shapes_with_width(3.07).await;
     match shapes_result {
         Ok(shapes) => {
             for shape in shapes {
